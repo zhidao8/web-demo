7c45203b1603ddc570a4259c2473a5f8c7cad872
zhidao8 <zhidao8carter@gmail.com>
2018-11-27 16:48:07 +0800

    播放按钮放在“光盘”上

--- music-wave/js/song-play.js
+++ music-wave/js/song-play.js
@@ -1,19 +1,40 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+  var pingingClass = 'playing'
+  var isPlaying = false
+
   var bgMusic = document.getElementsByClassName('bg-music')[0];
-  bgMusic.addEventListener('playing', function () {
+  bgMusic.addEventListener('play', function () {
     var hoverWrap = document.getElementsByClassName('hover-wrap')[0];
-    hoverWrap.classList.add('playing');
+    hoverWrap.classList.add(pingingClass);
     hoverWrap = null;
 
-    var coverWrap = document.getElementsByClassName('cover-wrap')[0];
-    coverWrap.classList.add('playing');
+    var coverWrap = document.getElementsByClassName('disc')[0];
+    coverWrap.classList.add(pingingClass);
     coverWrap = null;
+
+    isPlaying = true
+  })
+
+  bgMusic.addEventListener('pause', function () {
+    var hoverWrap = document.getElementsByClassName('hover-wrap')[0];
+    hoverWrap.classList.remove(pingingClass);
+    hoverWrap = null;
+
+    var coverWrap = document.getElementsByClassName('disc')[0];
+    coverWrap.classList.remove(pingingClass);
+    coverWrap = null;
+
+    isPlaying = false
   })
 
   var hover = document.getElementsByClassName('hover')[0];
   hover.addEventListener('click', function () {
-    bgMusic.volume = 0.5;
-    bgMusic.play();
+    if (isPlaying) {
+      bgMusic.pause();
+    } else {
+      bgMusic.play();
+    }
   })
   hover = null;
 })
