fa573df57d574826a788acafc0c33939dfe05c2b
zhidao8 <zhidao8carter@gmail.com>
2018-11-28 09:47:59 +0800

    背景音乐 canplay 时才能播放

--- music-wave/js/song-play.js
+++ music-wave/js/song-play.js
@@ -4,37 +4,43 @@ document.addEventListener('DOMContentLoaded', function () {
   var isPlaying = false
 
   var bgMusic = document.getElementsByClassName('bg-music')[0];
-  bgMusic.addEventListener('play', function () {
-    var hoverWrap = document.getElementsByClassName('hover-wrap')[0];
-    hoverWrap.classList.add(pingingClass);
-    hoverWrap = null;
 
-    var coverWrap = document.getElementsByClassName('disc')[0];
-    coverWrap.classList.add(pingingClass);
-    coverWrap = null;
+  bgMusic.addEventListener('canplay', function () {
 
-    isPlaying = true
+    bgMusic.addEventListener('play', function () {
+      var hoverWrap = document.getElementsByClassName('hover-wrap')[0];
+      hoverWrap.classList.add(pingingClass);
+      hoverWrap = null;
+
+      var coverWrap = document.getElementsByClassName('disc')[0];
+      coverWrap.classList.add(pingingClass);
+      coverWrap = null;
+
+      isPlaying = true
+    })
+
+    bgMusic.addEventListener('pause', function () {
+      var hoverWrap = document.getElementsByClassName('hover-wrap')[0];
+      hoverWrap.classList.remove(pingingClass);
+      hoverWrap = null;
+
+      var coverWrap = document.getElementsByClassName('disc')[0];
+      coverWrap.classList.remove(pingingClass);
+      coverWrap = null;
+
+      isPlaying = false
+    })
+
+    var hover = document.getElementsByClassName('hover')[0];
+    hover.addEventListener('click', function () {
+      if (isPlaying) {
+        bgMusic.pause();
+      } else {
+        bgMusic.play();
+      }
+    })
+    hover = null;
+
   })
 
-  bgMusic.addEventListener('pause', function () {
-    var hoverWrap = document.getElementsByClassName('hover-wrap')[0];
-    hoverWrap.classList.remove(pingingClass);
-    hoverWrap = null;
-
-    var coverWrap = document.getElementsByClassName('disc')[0];
-    coverWrap.classList.remove(pingingClass);
-    coverWrap = null;
-
-    isPlaying = false
-  })
-
-  var hover = document.getElementsByClassName('hover')[0];
-  hover.addEventListener('click', function () {
-    if (isPlaying) {
-      bgMusic.pause();
-    } else {
-      bgMusic.play();
-    }
-  })
-  hover = null;
 })
